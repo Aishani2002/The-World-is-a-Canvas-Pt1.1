da29c21bfa363d15a4ca5272b82caec0118812f9
Aishani2002 <61320981+Aishani2002@users.noreply.github.com>
2020-04-20 15:47:54 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+
+var position,database;
+
+function setup(){
+    createCanvas(displayWidth-20,displayHeight-20);
+    database = firebase.database();
+}
+
+function draw(){
+    background("white");
+
+}
